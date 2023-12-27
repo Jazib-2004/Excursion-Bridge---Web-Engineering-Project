e3f04e62f1cea843c915747d082b3eb33e0d6700
IzzahAslam <114334813+IzzahAslam@users.noreply.github.com>
2023-12-27 12:56:50 -0800

    Update register.js

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -4,12 +4,17 @@ const touristcontroller = require("../controllers/tourist_controller");
 
 // router.post("/", authcontroller.register);
 
-
 router.post("/", (req, res) => {
 
-    const { tourId } = req;
-    console.log("Tour ID is succesfully retrieved in routes", tourId);
-    touristcontroller.register(req, res, tourId);
+    // const { tourId } = req;
+    // console.log("Tour ID is succesfully retrieved in routes", tourId);
+    // touristcontroller.register(req, res, tourId);
+
+    const { tourId, userId } = req;
+    console.log("Tour ID is successfully retrieved in routes", tourId);
+    console.log("User ID is successfully retrieved in routes", userId);
+
+    touristcontroller.register(req, res, tourId, userId);
 });
 
 module.exports = router;
